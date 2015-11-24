eca2db2ed5a3ef1cac4e69fbd7c49ea70868ded4
cheng10 <cheng10@ualberta.ca>
2015-11-24 00:44:51 -0700

    retab

--- p2/Tilecoder.py
+++ p2/Tilecoder.py
@@ -1,24 +1,24 @@
 numTilings = 8
-    
+	
 def tilecode(x,y,tileIndices):
-    # write your tilecoder here (5 lines or so)
-    i = 0
-    for i in range (0,numTilings):
-        a = (x+i*0.6/8)*10/6
-        b =(y+i*0.6/8)*10/6
-        tileIndices[i] = int(a)+11*int(b) + i * 121
-        
+	# write your tilecoder here (5 lines or so)
+	i = 0
+	for i in range (0,numTilings):
+		a = (x+i*0.6/8)*10/6
+		b =(y+i*0.6/8)*10/6
+		tileIndices[i] = int(a)+11*int(b) + i * 121
+		
 def printTileCoderIndices(x,y):
-    tileIndices = [-1]*numTilings
-    tilecode(x,y,tileIndices)
-    
-    print 'Tile indices for input (',x,',',y,') are : ', tileIndices
+	tileIndices = [-1]*numTilings
+	tilecode(x,y,tileIndices)
+	
+	print 'Tile indices for input (',x,',',y,') are : ', tileIndices
 
 printTileCoderIndices(0.1,0.1)
 printTileCoderIndices(4.0,2.0)
 printTileCoderIndices(5.99,5.99)
 printTileCoderIndices(4.0,2.1)
-    
+	
 # Why?
 # Ans 1: Because each map has 121 tiles, and the tile counting starts from 0, So
 # the first tiling runs from 0 to 120, and the scond will continue from 121 to 241
