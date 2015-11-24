d223a8cacf95cc9164d9d445068966e1cb7ab147
Stinn <justin_lu55@hotmail.com>
2015-11-23 22:50:40 -0700

    p2 part2

--- p2/Tilecoder.py
+++ p2/Tilecoder.py
@@ -4,7 +4,9 @@ def tilecode(x,y,tileIndices):
     # write your tilecoder here (5 lines or so)
     i = 0
     for i in range (0,numTilings):
-        tileIndices[i] = int((x+i*0.6/8)*10/6)+11*int((y+i*0.6/8)*10/6) + i * 121
+        a = (x+i*0.6/8)*10/6
+        b =(y+i*0.6/8)*10/6
+        tileIndices[i] = int(a)+11*int(b) + i * 121
         
 def printTileCoderIndices(x,y):
     tileIndices = [-1]*numTilings
