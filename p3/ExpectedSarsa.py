439aed83e1d2ac986dfc5a9fb5453fa0c7908a63
cheng10 <cheng10@ualberta.ca>
2015-12-06 17:08:27 -0700

    draft version

--- p3/ExpectedSarsa.py
+++ p3/ExpectedSarsa.py
@@ -1,3 +1,5 @@
+#borrow from example code from course forum
+
 import blackjack
 import numpy as np
 
@@ -5,7 +7,7 @@ numStates  = 180
 numActions =   2
 gamma = 1.0
 
-numEpisodesLearn =  1000000
+numEpisodesLearn =	1000000
 numEpisodesEval  = 10000000
 alpha = 1e-3
 eps_mu = 1e-2
@@ -15,20 +17,20 @@ Q[-1] = np.zeros((numActions))
 
 returnSum = 0.0
 for episodeNum in xrange(numEpisodesLearn):
-    G = 0.0
-    s = blackjack.init()
-    while (s != -1):
-        a = np.argmax(Q[s]) if np.random.random() > eps_mu \
-            else np.random.randint(numActions)
-        (r, sp) = blackjack.sample(s, a)
-        v_pi = eps_pi * np.average(Q[sp]) + (1 - eps_pi) * np.max(Q[sp])
-        Q[s, a] += alpha * (r + gamma * v_pi - Q[s, a])
-        G = r + gamma * G
-        s = sp
-    returnSum += G
-    ep = episodeNum + 1 
-    if (ep % 10000 == 0):
-        print "Episode: ", ep, "Average return: ", returnSum / ep
+	G = 0.0
+	s = blackjack.init()
+	while (s != -1):
+		a = np.argmax(Q[s]) if np.random.random() > eps_mu \
+			else np.random.randint(numActions)
+		(r, sp) = blackjack.sample(s, a)
+		v_pi = eps_pi * np.average(Q[sp]) + (1 - eps_pi) * np.max(Q[sp])
+		Q[s, a] += alpha * (r + gamma * v_pi - Q[s, a])
+		G = r + gamma * G
+		s = sp
+	returnSum += G
+	ep = episodeNum + 1 
+	if (ep % 10000 == 0):
+		print "Episode: ", ep, "Average return: ", returnSum / ep
 print "Average return while learning: ", returnSum / numEpisodesLearn
 
 greedy = lambda s: np.argmax(Q[s])
@@ -36,10 +38,10 @@ blackjack.printPolicy(greedy)
 
 returnSum = 0.0
 for episodeNum in xrange(numEpisodesEval):
-    G = 0.0
-    s = blackjack.init()
-    while (s != -1):
-        (r, s) = blackjack.sample(s, greedy(s))
-        G = r + gamma * G
-    returnSum += G
+	G = 0.0
+	s = blackjack.init()
+	while (s != -1):
+		(r, s) = blackjack.sample(s, greedy(s))
+		G = r + gamma * G
+	returnSum += G
 print "Average return on deterministic policy: ", returnSum / numEpisodesEval
--- p3/learning-starter.py
+++ p3/learning-starter.py
@@ -1,34 +1,80 @@
-import mountaincar
+import mountaincar as mc
+import numpy as np
 from Tilecoder import numTilings, tilecode, numTiles
 from Tilecoder import numTiles as n
 from pylab import *  #includes numpy
 
-numRuns = 1
+numRuns = 50
 numEpisodes = 200
-alpha = 005/numTilings
+numActions=3
+alpha = 0.5/numTilings
 gamma = 1
 lmbda = 0.9
 Epi = Emu = epsilon = 0
-n = numTiles * 3
-F = [-1]*numTilings
+n = numTiles*numTiles*numActions
+F = [-1]*np.ones(numTilings)
+steps=np.zeros(numEpisodes)
+returns=np.zeros(numEpisodes)
 
 runSum = 0.0
 for run in xrange(numRuns):
 	theta = -0.01*rand(n)
 	returnSum = 0.0
 	for episodeNum in xrange(numEpisodes):
-		G = 0
-		...
-		your code goes here (20-30 lines, depending on modularity)
-		...
+		G = 0	
+#	your code goes here (20-30 lines, depending on modularity)
+		step=0
+		e=np.zeros(n)
+		s=mc.init()
+		Q=np.zeros(numActions)
+		while s!=None:
+			step=step+1
+			tilecode(s[0],s[1],F)
+			Q=np.zeros(numActions)
+			for a in range(3):
+				for _ in F:
+					Q[a]=Q[a]+theta[_+a*324]
+			a=np.argmax(Q)
+			r, s1=mc.sample(s,a)
+			G+=r
+			delta=r-Q[a]
+			for i in F:
+				e[i+a*324]=1
+			if s1==None:
+				for i in range(n):
+					theta[i]=theta[i]+alpha*delta*e[i]
+				break
+			tilecode(s1[0],s1[1],F)
+			Q=np.zeros(numActions)
+			for a in range(3):
+				for i in F:
+					Q[a]=Q[a]+theta[i+a*324]
+			delta=delta+np.max(Q)
+			for _ in range(n):
+				theta[_]=theta[_]+alpha*delta*e[_]
+				e[_]=lmbda*e[_]
+			s=s1
+		
 		print "Episode: ", episodeNum, "Steps:", step, "Return: ", G
 		returnSum = returnSum + G
+		steps[episodeNum]=steps[episodeNum]+step
+		returns[episodeNum]=returns[episodeNum]+G
 	print "Average return:", returnSum/numEpisodes
 	runSum += returnSum
 print "Overall performance: Average sum of return per run:", runSum/numRuns
 
 #Additional code here to write average performance data to files for plotting...
 #You will first need to add an array in which to collect the data
+def writeD():
+	f_mean=open('average.dat', 'w')
+	f_step=open('steps.dat', 'w')
+	for _ in range(numEpisodes):
+		f_mean.write(repr(_) + ' ' + repr(returns[_]/numRuns))
+		f_step.write(repr(_) + ' ' + repr(steps[_]/numRuns))
+		f_mean.write('\n')
+		f_step.write('\n')
+	f_step.close()
+	f_mean.close()
 
 def writeF():
 	fout = open('value', 'w')
